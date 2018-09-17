3133a556198e5725db1fde7a2c1d070d58474de8
Hernan <>
2018-09-16 20:59:37 -0500

    Version casi final
    (pequeño desfase de 4 metros en el area)

--- Main/src/Ejercicio2/Main.java
+++ Main/src/Ejercicio2/Main.java
@@ -1,10 +1,11 @@
 package Ejercicio2;
 
 import becker.robots.*;
+import javax.swing.JOptionPane;
 
 /**
  * Ejercicios con el robot Karel
- * @author Juan Diego Carrero Guerrero
+ * @author Hernan Javier Quiroga Bernal
  */
 public class Main {
     public static City objetos;
@@ -13,48 +14,63 @@ public class Main {
     public static void main(String[] args) {
         objetos = new City("Field2.txt");
         objetos.showThingCounts(true);
-        estudiante = new Robot(objetos,10, 2, Direction.WEST,0);
+        estudiante = new Robot(objetos, 10, 2, Direction.WEST, 1);
+        estudiante.putThing();
+        estudiante.move();
         int paredes = 0;
         int[][] area = null;
         
-        
-        int posInicialX = estudiante.getAvenue();
         int posInicialY = estudiante.getStreet();
         int posFinalX = 0;
-        int posFinalY = 0;
-      
+        
+        
         while(estudiante.canPickThing()==false){
             estudiante.turnLeft();
             estudiante.turnLeft();
             estudiante.turnLeft();
             while(estudiante.frontIsClear() == false){
                 estudiante.turnLeft();
-                 paredes++;
+                paredes++;
             }
-            //Le indica al robot cuando crear la matriz
-            if(estudiante.getAvenue() == posInicialY){
-                area = new int[estudiante.getAvenue()][estudiante.getStreet()];         
-                posFinalX = estudiante.getAvenue();
-                posFinalY = estudiante.getStreet();
+            if(estudiante.getStreet() == 0){
+                    posFinalX = estudiante.getAvenue();
             }
             estudiante.move();
         }
-        System.out.println("Creada matriz de tamaño : " + posFinalX + "," + posFinalY);
         
+        area = new int[posFinalX + 1][posInicialY + 1];
         
-        //Iguala toda la matriz a 0
-        for(int i = 0; i < posFinalX; i++){
-            for (int j = 0; j < posFinalY; j++) {
+        for (int i = 0; i <= posFinalX; i++) {
+            for (int j = 0; j <= posInicialY; j++) {
                 area[i][j] = 0;
             }
         }
         
-        //Imprime la matriz creada
-        for(int i = 0; i < posFinalX; i++){
-            for(int j = 0; j < posFinalY; j++){
-                System.out.print(area[i][j] + " ");
+        estudiante.move();
+        
+        while(estudiante.canPickThing()==false){
+            estudiante.turnLeft();
+            estudiante.turnLeft();
+            estudiante.turnLeft();
+            while(estudiante.frontIsClear() == false){
+                estudiante.turnLeft();
+                area[estudiante.getAvenue()][estudiante.getStreet()] = 1;
+            }
+            area[estudiante.getAvenue()][estudiante.getStreet()] = 1;
+            estudiante.move();
+        }
+        area[estudiante.getAvenue()][estudiante.getStreet()] = 1;
+         
+        int areaTotal = 0;
+        
+        for (int i = 1; i < posFinalX; i++) {
+            for (int j = 1; j < posInicialY; j++) {
+                if(area[j][i]==0)areaTotal++;
+                System.out.print(area[j][i] + " ");
             }
             System.out.print("\n");
         }
+        
+        JOptionPane.showMessageDialog(null, "El area de la zona es: " + areaTotal + "m^2");
     }
 }
